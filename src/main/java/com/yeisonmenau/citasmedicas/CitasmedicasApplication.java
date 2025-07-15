b616d7633fb4bb946d1bf7954586e7a1bed6fc67
yeisonmenau <yeisonandresdh@gmail.com>
2025-07-14 19:42:05 -0500

    Crar proyecto con dependencias básicas

---
+++ src/main/java/com/yeisonmenau/citasmedicas/CitasmedicasApplication.java
@@ -0,0 +1,13 @@
+package com.yeisonmenau.citasmedicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasmedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasmedicasApplication.class, args);
+	}
+
+}
